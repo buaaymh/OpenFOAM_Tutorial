d184f5dbf7ff525b9d14ace6acf0a5b01cb6c6bb
buaaymh <yangminghao@buaa.edu.cn>
2022-01-15 16:30:47 +0800

    Add test for coded BC and Initialized solutions.

---
+++ OF_Turorial_08/OF_Tutorial_08.C
@@ -0,0 +1,97 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    OF_Tutorial_08
+
+Description
+    This is the OpenFOAM CodeStream tutorial from buaaymh.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    // Initialise OF case
+    #include "setRootCaseLists.H"
+    // #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    Info<< "Reading field p\n" << endl;
+    volScalarField p // note that pressure is a scalar field
+	(
+		IOobject
+		(
+		    "p", // name of the field
+		    runTime.timeName(), // name of the current time, i.e. the time folder to read from
+		    mesh,
+		    IOobject::MUST_READ, // always gets imported, will throw an error if the field is missing
+		    IOobject::AUTO_WRITE // will get saved automatically when the controlDict parameters will request it
+		),
+		mesh // initialises the field to match the size of the mesh with default (0) values
+	);
+
+    Info<< "Reading field U\n" << endl;
+	volVectorField U // note that velocity is a vector field
+	(
+		IOobject
+		(
+		    "U",
+		    runTime.timeName(),
+		    mesh,
+		    IOobject::MUST_READ,
+		    IOobject::AUTO_WRITE
+		),
+		mesh
+	);
+
+    runTime++;
+
+    U.correctBoundaryConditions();
+
+    Info << "\nPatch movingWall:" << endl;
+    word findAPatch("movingWall");
+    label patchNum = mesh.boundary().findPatchID(findAPatch);
+    const vectorField& U_patch = U.boundaryField()[patchNum];
+    forAll(U_patch, faceI)
+    {
+        Info << U_patch[faceI] << endl;
+    }
+    Info << endl;
+
+    Info << "Time now = " << runTime.timeName() << endl << endl;
+
+    Info << "Fields have been written." << endl << endl;
+
+    runTime.write();
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
